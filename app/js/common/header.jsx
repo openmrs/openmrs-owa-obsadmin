5f7df49e907bf14858f6ead7ef39a18b71b3f8f3
Eunice Wanjiru <eunice.wanjiru@andela.com>
2017-10-04 18:58:39 +0300

    [PDMO-51] harmonize header fonts and add locations

--- app/js/common/header.jsx
+++ app/js/common/header.jsx
@@ -8,97 +8,182 @@
  * graphic logo is a trademark of OpenMRS Inc.
  */
 
-import React, {Component} from 'react';
-import {Link, IndexLink} from 'react-router';
+import React, { Component } from 'react';
+import { Link, IndexLink } from 'react-router';
 import apiCall from '../utilities/apiHelper';
 
-/**
- * Represents the menu bar.
- * 
- * @class Header
- * @extends {Component}
- */
 export default class Header extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       locationTags: [],
       currentLocationTag: "",
+      currentLocation: "",
       currentUser: "",
       currentLogOutUrl: "",
+      contextPath: "",
+      columns: 0,
+      selected: "",
     };
     this.getUri = this.getUri.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.locationsDropDown = this.locationsDropDown.bind(this);
   }
 
-  /**
-   * Gets the OpenMrs configuration and sets the logout link
-   * @memberOf Header
-   */
   componentWillMount() {
-    apiCall(null, 'get', '/location').then((response) => {
-      this.setState({locationTags: response.results});
-      this.getUri();
-    });
+    apiCall(null, 'get', '/location?tag=Login%20Location')
+      .then((response) => {
+        this.setState({ locationTags: response.results },
+          () => {
+            const columns = Math.floor(this.state.locationTags.length / 3);
+            this.setState({
+              columns
+            });
+          }
+        );
+        this.getUri();
+      });
 
-    apiCall(null, 'get', '/session').then((response) => {
-      this.setState({currentUser: response.user.display});
-    });
+    apiCall(null, 'get', 'appui/session?v=full')
+      .then((response) => {
+        const { user, sessionLocation } = response;
+        this.setState({
+          currentUser: user && user.display,
+          currentLocationTag: sessionLocation && sessionLocation.uuid,
+          currentLocation: sessionLocation && sessionLocation.display,
+          selected: sessionLocation && sessionLocation.display,
+        });
+      });
+
+    const host = location.href.split('/')[3];
+    const locationorigin = location.origin;
+    const contextPath = locationorigin + '/' + host;
+    this.setState({ contextPath })
   }
 
-  /**
-   * Gets logout Url 
-   * 
-   * @returns {string} - logout url
-   */  
   getUri() {
     this.state.locationTags.map((location) => {
       let url = location.links[0].uri;
       let arrUrl = url.split("/");
       let customUrl = `/${arrUrl[3]}/appui/header/logout.action?successUrl=${arrUrl[3]}`;
-      this.setState({currentLogOutUrl: customUrl});
+      this.setState({ currentLogOutUrl: customUrl });
       return customUrl;
-        
     });
   }
 
-  /**
-   * Renders the component
-   * 
-   * @memberOf Header
-   */
+  handleLocationChange(event) {
+    event.preventDefault();
+    this.setState({
+      currentLocationTag: event.target.id,
+      currentLocation: event.target.name,
+      selected: event.target.name,
+    }, () => {
+      apiCall({ 'location': this.state.currentLocationTag }, 'post', 'appui/session')
+        .then((response) => {
+          (response.error) ? toastr.error(response.error) : ''
+        })
+        .catch(error => error)
+    });
+  }
+
+  locationsDropDown() {
+    const columnsData = (locationTags, numberOfColumns) => locationTags
+      .reduce((columns, location) => {
+        const lastColumn = columns[columns.length - 1];
+        if (lastColumn.length < numberOfColumns) {
+          return [...columns.slice(0, -1), [...lastColumn, location]]
+        }
+        return [...columns, [location]]
+      }, [[]]);
+    return columnsData;
+  }
+
   render() {
+    const locations = this.locationsDropDown();
     return (
-        <header>
-            <div className="logo">
-                <a href="../../">
-                    <img src="img/openmrs-with-title-small.png"/>
+      <header >
+        <div className="logo">
+          <a href="../../">
+            <img src="img/openmrs-with-title-small.png" />
+          </a>
+        </div>
+
+        <ul className="navbar-right nav-header">
+          <Link to="" activeClassName="active">
+            <li className="dropdown">
+              <a
+                id="user-drop"
+                className="dropdown-toggle"
+                data-toggle="dropdown"
+                href="#"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i className="fa fa-user" /> {' ' + this.state.currentUser}
+                <span className="caret" />
+              </a>
+              <ul className="dropdown-menu user">
+                <a
+                  className="account"
+                  href={this.state.contextPath + "/adminui/myaccount/myAccount.page"}
+                >My Account
                 </a>
-            </div>
+              </ul>
+            </li>
+          </Link>
 
-            <ul className="navbar-right nav-header">
-                <Link to="" activeClassName="active">
-                    <li className="dropdown">
-                        <a className="dropdown-toggle" data-toggle="dropdown" 
-                          href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                          <span className="glyphicon glyphicon-user"/> {' ' + this.state.currentUser}
-                          <span className="caret"/>
+          <li className="dropdown">
+            <a
+              id="locations-drop"
+              className="dropdown-toggle"
+              data-toggle="dropdown"
+              href="#"
+              role="button"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <i className="fa fa-map-marker" />
+              {(this.state.currentLocationTag !== "")
+                ? this.state.currentLocation
+                : ''}
+              <span className="caret" />
+            </a>
+
+            <ul className="dropdown-menu locations">
+              {locations(this.state.locationTags, this.state.columns)
+                .map((locationColumn, index) => (
+                  <ul className="row flex-column" key={index}>
+                    {locationColumn.map((location, index) => (
+                      <li
+                        key={index}
+                        id="location"
+                        className={
+                          (this.state.selected === location.display)
+                            ? 'selected' : ''}
+                      >
+                        <a
+                          className="col col-3"
+                          id={location.uuid}
+                          name={location.display}
+                          onClick={this.handleLocationChange}
+                        >{location.display}
                         </a>
-                        <ul className="dropdown-menu user">
-                            <li>
-                                <a href="#">My Account</a>
-                            </li>
-                        </ul>                        
-                    </li>
-                </Link>
-
-                <Link to="" activeClassName="active">
-                    <li>
-                        <a href={this.state.currentLogOutUrl}>Logout {' '}
-                            <span className="glyphicon glyphicon-log-out"/></a>
-                    </li>
-                </Link>
+                      </li>
+                    ))}
+                  </ul>
+                ))}
             </ul>
-        </header>
+          </li>
+
+          <Link to="" activeClassName="active">
+            <li className="logout">
+              <a href={this.state.currentLogOutUrl}>Logout {' '}
+                <i className="fa fa-sign-out" /></a>
+            </li>
+          </Link>
+        </ul>
+      </header>
     );
   }
 }
